e322fd0a9233930559c61a8cfc92b0f8b3471cad
Daniel dias <43478316+dani3434@users.noreply.github.com>
2023-12-14 16:41:04 -0300

    first commit

---
+++ src/common/errors/interceptors/database.interceptor.ts
@@ -0,0 +1,29 @@
+import {
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+  BadRequestException,
+} from '@nestjs/common';
+import { catchError, Observable } from 'rxjs';
+import { isPrimaError } from '../utils/is-prisma-error.util';
+import { handleDatabaseErrors } from '../utils/handle-database-error.util';
+import { DatabaseError } from '../types/DatabaseError';
+
+@Injectable()
+export class DatabaseInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    return next.handle().pipe(
+      catchError(error => {
+        if (isPrimaError(error)) {
+          error = handleDatabaseErrors(error);
+        }
+        if (error instanceof DatabaseError) {
+          throw new BadRequestException(error.message);
+        } else {
+          throw error;
+        }
+      }),
+    );
+  }
+}
